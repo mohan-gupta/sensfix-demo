6af9d3c406e62ad5468227b7c1cb453fd86067e5
Mihir Inamdar <39341893+inamdarmihir@users.noreply.github.com>
2023-10-12 17:16:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,30 +11,30 @@ from first_classification_prompt import get_first_classification_prompt
 from second_classification_prompt import get_second_classification_prompt
 
 @app.get("/ticket_qualification")
-async def categorize_and_respond(user_complaint: str):
+async def categorize_and_respond(user_input: str):
     # Validation
-    validation_result = validation_chain.run(user_complaint)
+    validation_result = validation_chain.run(user_input)
 
-    if validation_result == "Incomplete":
+    if validation_result == "output:Incomplete":
         return {"status": "incomplete"}
 
     # First classification
     first_classification_prompt = get_first_classification_prompt()
     first_category_chain = LLMChain(llm=llm2, prompt=first_classification_prompt)
-    first_category = await asyncio.to_thread(first_category_chain.run, user_complaint)
+    first_category = await asyncio.to_thread(first_category_chain.run, user_input)
 
     # Second classification
     second_classification_prompt = get_second_classification_prompt()
     second_category_chain = LLMChain(llm=llm3, prompt=second_classification_prompt)
-    second_category = await asyncio.to_thread(second_category_chain.run, user_complain=first_category)
+    second_category = await asyncio.to_thread(second_category_chain.run, user_input=first_category)
 
     # Ticket generation
     ticket_chain = LLMChain(llm=llm1, prompt=ticket_prompt)
-    ticket_result = await asyncio.to_thread(ticket_chain.run, user_complaint)
+    ticket_result = await asyncio.to_thread(ticket_chain.run, user_input)
 
     # Response generation
     response_chain = LLMChain(llm=llm2, prompt=response_prompt)
-    response = await asyncio.to_thread(response_chain.run, user_complaint=user_complaint)
+    response = await asyncio.to_thread(response_chain.run, user_input=user_input)
 
     return {
         "ticket_status": ticket_result,
