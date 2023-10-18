60c9593cd9b0f7319ae07273229448d263699dd3
Mihir Inamdar <39341893+inamdarmihir@users.noreply.github.com>
2023-10-18 11:20:58 +0530

    Update clf_api.py

--- clf_api.py
+++ clf_api.py
@@ -1,27 +1,25 @@
 from fastapi import APIRouter
-
 from langchain import LLMChain
-
 from dependencies import llm1, llm2, llm3
-
 from prompts import (
     validation_prompt,
     category_l1_prompt,
     category_l2_prompt,
     ticket_prompt,
-    response_prompt
+    response_prompt,
+    response_prompt_korean,
+    response_prompt_spanish
 )
-
 from prompt_example import get_valid, get_category_l1, get_category_l2, get_ticket, get_response
 
 router = APIRouter()
 
 @router.get("/ticket_qualification")
-async def categorize_and_respond(user_input: str):
+async def categorize_and_respond(user_input: str, language: str = "english", memory: str = ""):
     # Validation
     valid_eg = get_valid()
     validation_chain = LLMChain(llm=llm3, prompt=validation_prompt)
-    validation_result = validation_chain.run(examples = valid_eg, user_input=user_input)
+    validation_result = validation_chain.run(examples=valid_eg, user_input=user_input)
 
     if validation_result == "incomplete":
         return {"status": "incomplete"}
@@ -34,7 +32,7 @@ async def categorize_and_respond(user_input: str):
     # Level 2 classification
     category_lst = list(map(str.title, category_l1.split("/")))
     categories = " or ".join(category_lst)
-    
+
     category_l2_eg = get_category_l2(category_lst)
     category_l2_chain = LLMChain(llm=llm3, prompt=category_l2_prompt)
     category_l2 = category_l2_chain.run(categories=categories, examples=category_l2_eg, user_input=user_input)
@@ -44,10 +42,20 @@ async def categorize_and_respond(user_input: str):
     ticket_chain = LLMChain(llm=llm1, prompt=ticket_prompt)
     ticket_result = ticket_chain.run(examples=ticket_eg, user_input=user_input)
 
-    # Response generation
+    # Provide context for the conversation
+    context = f"{user_input}\n{memory}"  # Combine the user's input and memory
+
+    # Response generation based on language choice
     response_eg = get_response(category_l2)
-    response_chain = LLMChain(llm=llm2, prompt=response_prompt)
-    response = response_chain.run(examples=response_eg, user_input=user_input)
+
+    if language.lower() == "korean":
+        response_chain = LLMChain(llm=llm2, prompt=response_prompt_korean)
+    elif language.lower() == "spanish":
+        response_chain = LLMChain(llm=llm2, prompt=response_prompt_spanish)
+    else:
+        response_chain = LLMChain(llm=llm2, prompt=response_prompt)
+
+    response = response_chain.run(examples=response_eg, user_input=context)
 
     return {
         "ticket_status": ticket_result,
